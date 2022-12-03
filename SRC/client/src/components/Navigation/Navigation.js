b6e58e21ac2f844d50729f9821f214b3a54c18b1
Brayden Thompson <bthomp89@uwo.ca>
2022-12-03 14:12:43 -0500

    nav bar

---
+++ SRC/client/src/components/Navigation/Navigation.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Nav, Navbar, NavLink } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Navigation() {
+  return (
+    <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+      <Navbar.Toggle
+        aria-controls="navbarScroll"
+        data-bs-target="#navbarScroll"
+      />
+      <Navbar.Collapse>
+        <Nav>
+          <NavLink eventKey="1" as={Link} to="/">
+            TEST
+          </NavLink>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
